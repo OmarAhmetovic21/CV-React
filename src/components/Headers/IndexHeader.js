616c1b0cbfadee57a4e9663a92163765f9e8c3fc
OmarAhmetovic21 <omarahmetovic19@gmail.com>
2023-06-27 12:58:55 +0200

    Navbar work

--- src/components/Headers/IndexHeader.js
+++ src/components/Headers/IndexHeader.js
@@ -28,7 +28,7 @@ function IndexHeader() {
         <div
           className="page-header-image"
           style={{
-            backgroundImage: "url(" + require("assets/img/header.jpg") + ")"
+            backgroundImage: "url(" + require("assets/img/IT.jpg") + ")"
           }}
           ref={pageHeader}
         ></div>
